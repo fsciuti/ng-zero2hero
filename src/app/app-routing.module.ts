76514830241cdc08d076a7df350869a72c8939ad
fsciuti <fsciuti@gmail.com>
2020-01-13 18:48:24 +0100

    feat: add project-detail route and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProjectContainerComponent } from './projects/project-container/project-container.component';
+import { ProjectDetailComponent } from './projects/project-detail/project-detail.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+    { path: 'projects/detail', component: ProjectDetailComponent },
     { path: 'projects', component: ProjectContainerComponent },
     { path: 'home', component: HomeComponent },
     { path: '',   redirectTo: '/home', pathMatch: 'full' }
