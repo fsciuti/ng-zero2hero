c55e76b6429881f7579b1abc3a5e1b235334b452
fsciuti <fsciuti@gmail.com>
2020-01-13 17:57:44 +0100

    feat: update project-list.component

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Project } from '../models/Project';
 
 @Component({
   selector: 'ngptt-project-list',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent implements OnInit {
+  @Input() projects: Project[] = [];
+  @Output() selected = new EventEmitter<Project>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  select(project: Project) {
+    this.selected.emit(project);
+  }
 }
