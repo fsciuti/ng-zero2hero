e6705fac11bc1fcc3b4947ecf77c3e0ed9d8f2ee
fsciuti <fsciuti@gmail.com>
2020-01-13 17:27:21 +0100

    feat: update project-detail.comp w/ input binding

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Project } from '../models/Project';
 
 @Component({
   selector: 'ngptt-project-detail',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-detail.component.css']
 })
 export class ProjectDetailComponent implements OnInit {
+  @Input() project: Project;
 
   constructor() { }
 
