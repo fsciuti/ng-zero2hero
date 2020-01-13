ed739882146aa8b914ed039c4c70c6344d8bb8ae
fsciuti <fsciuti@gmail.com>
2020-01-13 17:36:07 +0100

    feat: update project-form.comp w/ EventEmitter

--- src/app/project-form/project-form.component.ts
+++ src/app/project-form/project-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Project } from '../models/Project';
 
 @Component({
   selector: 'ngptt-project-form',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-form.component.css']
 })
 export class ProjectFormComponent implements OnInit {
+  @Output() submitted = new EventEmitter<Project>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  submitProjectForm(form: NgForm) {
+    this.submitted.emit({
+        ...form.value
+    });
+  }
+  
 }
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -59,13 +59,13 @@ export class ProjectComponent implements OnInit {
     this.selectedProject = project;
   }
 
-  submitProjectForm(form: NgForm) {
+  addNewProject(project: Project) {
     this.projects.push({
       id: this.projects.length,
       code: Math.random().toString(36).replace('0.', '').substring(2, 9),
       done: false,
       tasks: [],
-      ...form.value
+      ...project
     });
   }
 }
