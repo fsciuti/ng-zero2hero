08423fe52e07028202d32daf9775d497c6128c77
fsciuti <fsciuti@gmail.com>
2020-01-13 16:12:40 +0100

    feat: add project.component

---
+++ src/app/project/project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngptt-project',
+  templateUrl: './project.component.html',
+  styles: []
+})
+export class ProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
