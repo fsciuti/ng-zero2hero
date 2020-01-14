d8aafc388662caca4cf0106f17e966b5b436eed0
fsciuti <fsciuti@gmail.com>
2020-01-14 09:51:21 +0100

    feat: update app-routing project-detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ProjectDetailComponent } from './projects/project-detail/project-detail
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-    { path: 'projects/detail', component: ProjectDetailComponent },
+    { path: 'projects/detail/:id', component: ProjectDetailComponent },
     { path: 'projects', component: ProjectContainerComponent },
     { path: 'home', component: HomeComponent },
     { path: '',   redirectTo: '/home', pathMatch: 'full' }
